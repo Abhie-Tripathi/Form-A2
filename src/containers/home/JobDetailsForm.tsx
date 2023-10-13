d2c7f973af2f0c8c093ef019d72ff1525958d048
Abhinav Tripathi <abhinav.tripathi@greedygame.com>
2023-10-13 11:41:41 +0530

    Final Commit

---
+++ src/containers/home/JobDetailsForm.tsx
@@ -0,0 +1,105 @@
+import { Button, Flex, Box } from "@chakra-ui/react";
+import React from "react";
+import FormInput from "../../components/formComponents/FormInput";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { PageNumbers } from "../../interface/home";
+import { IJobDetails } from "../../interface/forms";
+import { useData } from "./DataProvider";
+
+const JobDetailsForm: React.FC<{
+  handleTab: (n: PageNumbers) => void;
+}> = ({ handleTab }) => {
+  const { handleChange, errors, touched, handleBlur, handleSubmit, values } =
+    useFormik<IJobDetails>({
+      initialValues: {
+        jobTitle: "",
+        jobDetails: "",
+        jobLocation: "",
+      },
+      validationSchema: Yup.object().shape({
+        jobTitle: Yup.string().required("Job Title is required"),
+        jobDetails: Yup.string().required("Job Details is required"),
+        jobLocation: Yup.string().required("Job Location is required"),
+        // jobPosition: Yup.string().required("Job position is required"),
+      }),
+      onSubmit: (values) => {
+        handleTab(2);
+      },
+    });
+
+  const dataContext = useData();
+
+  const updateField = (
+    e: React.ChangeEvent<HTMLInputElement>,
+    data: Partial<IJobDetails>
+  ) => {
+    handleChange(e);
+    dataContext?.setState((prev) => ({
+      ...prev,
+      jobDetails: {
+        ...prev.jobDetails,
+        ...data,
+      },
+    }));
+  };
+
+  return (
+    <Box width="100%" as="form" onSubmit={handleSubmit as any}>
+      <Box width="100%">
+        <FormInput
+          label="Job Title"
+          placeholder="Enter job title"
+          name="jobTitle"
+          onChange={(e) =>
+            updateField(e, {
+              jobTitle: e.target.value,
+            })
+          }
+          onBlur={handleBlur}
+          value={values?.jobTitle}
+          error={errors?.jobTitle}
+          touched={touched?.jobTitle}
+        />
+        <FormInput
+          label="Job Details"
+          placeholder="Enter job details"
+          name="jobDetails"
+          onChange={(e) =>
+            updateField(e, {
+              jobDetails: e.target.value,
+            })
+          }
+          onBlur={handleBlur}
+          value={values?.jobDetails}
+          error={errors?.jobDetails}
+          touched={touched?.jobDetails}
+        />
+        <FormInput
+          label="Job Location"
+          name="jobLocation"
+          placeholder="Enter job location"
+          onChange={(e) =>
+            updateField(e, {
+              jobLocation: e.target.value,
+            })
+          }
+          onBlur={handleBlur}
+          error={errors.jobLocation}
+          touched={touched.jobLocation}
+          value={values.jobLocation}
+        />
+        <Flex w="100%" justify="flex-end" mt="4rem" gap="20px">
+          <Button colorScheme="gray" type="button" onClick={() => handleTab(0)}>
+            Previous
+          </Button>
+          <Button colorScheme="red" type="submit">
+            Next
+          </Button>
+        </Flex>
+      </Box>
+    </Box>
+  );
+};
+
+export default JobDetailsForm;
